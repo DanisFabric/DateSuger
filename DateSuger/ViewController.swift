e541d6dd8c2efb53432025f4be328b97493d20c9
DanisFabric <danisfabric@gmail.com>
2015-12-13 17:10:03 +0800

    Initial Commit

---
+++ DateSuger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DateSuger
+//
+//  Created by Danis on 15/12/13.
+//  Copyright © 2015年 danis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
